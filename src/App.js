527e1aed83e4a924026c068753e7dd488391b9bf
EliOlga <elisabeth.olga@gmail.com>
2022-12-17 14:42:44 -0500

    Afficher un resultat de recherche par titre
    (cherry picked from commit f45569e7910f06124245c859920e58bb76bdccf7)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
 
 	return (
 		<div>
-			<h1>API</h1>
+			<h1>MAC</h1>
 			<RechercheMusee/>
 		</div>
 	);
--- src/composants/RechercheMusee.js
+++ src/composants/RechercheMusee.js
@@ -2,47 +2,54 @@
 /*{libelleNomsArtistes, titre, dateProduction, categorie, materiaux, dimensions, lieuProduction, cultures, emplacementHorsMurs, provenance, collection, departement, dateAcquisition, numero, oeuvrePrincipale, elements, nomsArtistesTries, artistes, evenements, publications, id}*/
 
 import {useEffect, useState} from "react";
+import {Button, Input, Select} from "semantic-ui-react";
 
 const RechercheMusee = () => {
 
+	const [titre, setTitre] = useState("");
+	const [oeuvres, setOeuvres] = useState([]);
+//	const [isLoading, setIsLoading] = useState(true);
+
 	const appelApi = () => {
-			setIsLoading(true)
+		//		setIsLoading(true)
 
 		//event.preventDefault();
 		// get request
 //		fetch('https://macrepertoire.macm.org/api/oeuvres/')
 		fetch('https://www.donneesquebec.ca/recherche/dataset/3b475449-f7e0-4f68-b9e8-933dda1916d0/resource/6e08aa19-4653-411a-ab80-d97d4feaeba5/download/oeuvres-mac.json')
+			//		fetch('https://jsonplaceholder.typicode.com/posts')
 			.then((response) => response.json())
-			.then((data) => {
-				setOeuvres(data)
-							setIsLoading(false)
 
+			.then((data) => {
+				console.log(data);
+				setOeuvres(data)
+				trouverOeuvre();
 			});
+
 		//	.catch((erreur) => console.log(erreur));
 	}
-
-	const [oeuvres, setOeuvres] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-
 
 	useEffect(() => {
 		appelApi();
 	}, []);
 
-const tabOeuvres = [];
-
-	tabOeuvres.push(oeuvres.at(1));
-	tabOeuvres.push(oeuvres.at(2));
-	tabOeuvres.push(oeuvres.at(3));
+	function trouverOeuvre() {
+		oeuvres.forEach(function (element) {
+		//	console.log(element);
+			if (element.titre === titre){
+				console.log(element);
+			}
+		})
+	}
 
 
 	const renderOeuvres = () => {
-		return tabOeuvres.map((item) => {
+		return oeuvres.map((item) => {
 			return (
 				<div>
 					<ul>
 						<li key={item.numero}>
-							{item.titre}, {item.libelleNomsArtistes}, {item.dateProduction}
+							{item.titre}
 						</li>
 					</ul>
 				</div>
@@ -53,11 +60,16 @@ const tabOeuvres = [];
 
 	return (
 		<div>
+			<Select></Select>
 
-			<button onClick={renderOeuvres}>Appel API MAC</button>
+			<Input type="text" value={oeuvres.titre} onChange={(e) => setTitre(e.target.value)} placeholder="Titre de l'oeuvre"/>
+			{/*<Select options={oeuvres.numero} value={oeuvres.numero}/>*/}
+
+			<Button onClick={appelApi}>Recherche par titre</Button>
 			<div>
 				{renderOeuvres()}
 			</div>
+
 		</div>
 	)
 }
