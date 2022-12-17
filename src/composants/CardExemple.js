1a4404ccd69197853737ec12f7646d421851c622
EliOlga <elisabeth.olga@gmail.com>
2022-12-17 15:20:55 -0500

    Afficher Matthew
    (cherry picked from commit 6d4d93dda7dd45c41ec9b545be6b8eda53054b11)
    
    (cherry picked from commit 755bdd373a8399bcfb86c0b75bef04e3abd71688)

--- src/composants/CardExemple.js
+++ src/composants/CardExemple.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
-const CardExampleCard = (props) => (
+const CardExampleCard = () => (
 	<Card>
-		{console.log(props)}
-		{/*<Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />*/}
+		<Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
 		<Card.Content>
-			<Card.Header>{props.titre}</Card.Header>
+			<Card.Header>Matthew</Card.Header>
 			<Card.Meta>
-				{/*<span className='date'>Joined in 2015</span>*/}
+				<span className='date'>Joined in 2015</span>
 			</Card.Meta>
 			<Card.Description>
-				{props.dateProduction}
+				Matthew is a musician living in Nashville.
 			</Card.Description>
 		</Card.Content>
 		<Card.Content extra>
-		{/*	<a>
+			<a>
 				<Icon name='user' />
 				22 Friends
-			</a>*/}
+			</a>
 		</Card.Content>
 	</Card>
 )
