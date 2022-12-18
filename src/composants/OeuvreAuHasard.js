b7f689bd767dea81ab0a72fa26d480c2e3843757
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-18 15:07:01 -0500

    ajouter une page qui affiche une oeuvre au hasard
    (cherry picked from commit 0b9766dbfb0b7df0b49404b6787eae1bba451b83)

---
+++ src/composants/OeuvreAuHasard.js
@@ -0,0 +1,33 @@
+// valeurs disponibles dans le json:
+/*{libelleNomsArtistes, titre, dateProduction, categorie, materiaux, dimensions, lieuProduction, cultures, emplacementHorsMurs, provenance, collection, departement, dateAcquisition, numero, oeuvrePrincipale, elements, nomsArtistesTries, artistes, evenements, publications, id}*/
+
+import React, {useState} from "react";
+import {Button, Container} from "semantic-ui-react";
+import CardExemple from "./CardExemple";
+
+const OeuvreAuHasard = (props) => {
+
+   const [uneOeuvre, setUneOeuvre] = useState({})
+
+   let index = Math.floor(Math.random() * props.oeuvres.length);
+   const uneOeuvreHandler = () => {
+      setUneOeuvre(trouverOeuvreHasard());
+   }
+
+   function trouverOeuvreHasard() {
+      return props.oeuvres[index];
+   }
+
+   return (
+      <Container>
+
+
+         <Button onClick={uneOeuvreHandler}>Afficher une oeuvre au hasard</Button>
+         <div>
+            {uneOeuvre.titre !== undefined ? <CardExemple oeuvre={uneOeuvre}/> : undefined}
+         </div>
+
+      </Container>
+   )
+}
+export default OeuvreAuHasard;
