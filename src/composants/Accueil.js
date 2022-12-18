c4a810b0d7e1a3619121c223e617eab9348cbe3a
EliOlga <elisabeth.olga@gmail.com>
2022-12-17 23:46:28 -0500

    Créer un affichage des titres courts dans Accueil pour une recherche plus facile
    (cherry picked from commit db2d6bd1643520d6bee3ee578286f4782dc4b4f3)

--- src/composants/Accueil.js
+++ src/composants/Accueil.js
@@ -1,29 +1,39 @@
 import React, {useEffect, useState} from 'react'
+import {Link} from "react-router-dom";
+
 const Accueil = () => {
 
-    const [titre, setTitre] = useState("");
-    const [oeuvres, setOeuvres] = useState([]);
-    var tipit = {};
-    const [uneOeuvre, setUneOeuvre] = useState({})
+	const [oeuvres, setOeuvres] = useState([]);
 
-    const appelApi = () => {
-        fetch('https://www.donneesquebec.ca/recherche/dataset/3b475449-f7e0-4f68-b9e8-933dda1916d0/resource/6e08aa19-4653-411a-ab80-d97d4feaeba5/download/oeuvres-mac.json')
-            .then((response) => response.json())
+	const appelApi = () => {
+		fetch('https://www.donneesquebec.ca/recherche/dataset/3b475449-f7e0-4f68-b9e8-933dda1916d0/resource/6e08aa19-4653-411a-ab80-d97d4feaeba5/download/oeuvres-mac.json')
+			.then((response) => response.json())
 
-            .then((data) => {
-                console.log(data);
-                setOeuvres(data)
-            });
-    }
+			.then((data) => {
+				console.log(data);
+				setOeuvres(data)
+			});
+	}
 
-    useEffect(() => {
-        appelApi();
-    }, []);
+	useEffect(() => {
+		appelApi();
+	}, []);
 
-    return (
-        <div>
+	const renderOeuvres = () => {
+		return oeuvres.map((item) => {
+            if (item.titre.length<7) {
+                return (
+                    <Link to="/Recherche">{item.titre} / </Link>
+                )
+            }
+            else return null;
+		})
+	}
+	return (
+		<div>
+            <h1>Titres de moins de 7 caractères pour la recherche</h1>
+			{renderOeuvres()}
 
-
-        </div>)
+		</div>)
 }
 export default Accueil
