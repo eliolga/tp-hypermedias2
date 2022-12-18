b94f73921df142409ad3fbbe46bbf21a9a64b818
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-18 15:06:07 -0500

    ajouter une route
    (cherry picked from commit 422e4d61e771839cf0b5160faf088dfa16ad327b)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
-import React from "react";
+import React, {useEffect, useState} from "react";
 import RechercheMusee from "./composants/RechercheMusee";
 import MenuExampleAttached from "./composants/Menu";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Accueil from "./composants/Accueil";
 import {Container} from "semantic-ui-react";
 import Guide from "./composants/Guide";
+import OeuvreAuHasard from "./composants/OeuvreAuHasard";
 
 export default function App() {
+
+   const [oeuvres, setOeuvres] = useState()
+   const appelApi = () => {
+      fetch('https://www.donneesquebec.ca/recherche/dataset/3b475449-f7e0-4f68-b9e8-933dda1916d0/resource/6e08aa19-4653-411a-ab80-d97d4feaeba5/download/oeuvres-mac.json')
+         .then((response) => response.json())
+         .then((data) => {
+            setOeuvres(data)
+         });
+   }
+   useEffect(() => {
+      appelApi();
+   }, []);
 
    return (
       <BrowserRouter>
@@ -18,7 +31,8 @@ export default function App() {
 
             <Routes>
                <Route path="/" element={<Accueil/>}/>
-               <Route path="/Recherche" element={<RechercheMusee/>}/>
+               <Route path="/Recherche" element={<RechercheMusee oeuvres = {oeuvres}/>}/>
+               <Route path="/OeuvreAuHasard" element={<OeuvreAuHasard oeuvres = {oeuvres}/>} />
                <Route path="/Guide" element={<Guide/>}/>
             </Routes>
          </Container>
