988d4ec14df4de036039b095dda7522f238d2501
EliOlga <elisabeth.olga@gmail.com>
2022-12-17 23:15:41 -0500

    tester l'accueil
    (cherry picked from commit 6859ab23d3b411c976ce84cd1d89899e3af8dc96)

--- src/composants/Accueil.js
+++ src/composants/Accueil.js
@@ -1,7 +1,29 @@
-import React from 'react'
-const Accueil = () => (
-   <div>
-      <h1>Les hommes des cavernes faisaient de l’animisme</h1>
-   </div>)
+import React, {useEffect, useState} from 'react'
+const Accueil = () => {
 
+    const [titre, setTitre] = useState("");
+    const [oeuvres, setOeuvres] = useState([]);
+    var tipit = {};
+    const [uneOeuvre, setUneOeuvre] = useState({})
+
+    const appelApi = () => {
+        fetch('https://www.donneesquebec.ca/recherche/dataset/3b475449-f7e0-4f68-b9e8-933dda1916d0/resource/6e08aa19-4653-411a-ab80-d97d4feaeba5/download/oeuvres-mac.json')
+            .then((response) => response.json())
+
+            .then((data) => {
+                console.log(data);
+                setOeuvres(data)
+            });
+    }
+
+    useEffect(() => {
+        appelApi();
+    }, []);
+
+    return (
+        <div>
+
+
+        </div>)
+}
 export default Accueil
