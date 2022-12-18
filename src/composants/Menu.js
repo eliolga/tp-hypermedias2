fa7c41bd38dbff5876572cd33f597e7599bf7945
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-18 15:06:29 -0500

    ajouter un item dans le menu
    (cherry picked from commit 67800e3c91467628e9a3ca3bc055a887a5eede84)

--- src/composants/Menu.js
+++ src/composants/Menu.js
@@ -9,6 +9,7 @@ const MenuExampleAttached = () => (
 				<Dropdown.Menu>
 					<Dropdown.Item><NavLink to="/">Accueil</NavLink></Dropdown.Item>
 					<Dropdown.Item><NavLink to="/Recherche">Recherche</NavLink></Dropdown.Item>
+					<Dropdown.Item><NavLink to="/OeuvreAuHasard">Au hasard de l’art</NavLink></Dropdown.Item>
 					<Dropdown.Item><NavLink to="/Guide">Guide d’utilisation</NavLink></Dropdown.Item>
 				</Dropdown.Menu>
 			</Dropdown>
