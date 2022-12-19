f228ad98e052314b2baeca671fcca19c7372b0c8
EliOlga <elisabeth.olga@gmail.com>
2022-12-18 23:56:22 -0500

    Correction de typos

--- src/composants/Guide.js
+++ src/composants/Guide.js
@@ -6,10 +6,10 @@ const Guide = () => (
 		<h2>Bienvenue dans l’application du Musée d’art contemporain de Montréal</h2>
 
 		<p>Pour afficher une oeuvre et quelques informations à son propos, rendez-vous dans la section "Recherche".</p>
-		<p>Entrer le titre de l’oeuvre dans la barre à cet effet et appuyer sur le bouton.</p>
-		<p>Vous pouvez également consulté une oeuvre au hazard dans la section "Oeuvre au hasard"</p>
+		<p>Entrez le titre de l’oeuvre dans la barre à cet effet et appuyer sur le bouton.</p>
+		<p>Vous pouvez également consulter une oeuvre au hasard dans la section "Oeuvre au hasard"</p>
 
-		<p><em>Remarque: Vous trouverez de titre d’une sélection restreinte d’oeuvres sur notre page d’accueil</em></p>
+		<p><em>Remarque: Vous trouverez des titres d’une sélection restreinte d’oeuvres sur notre page d’accueil.</em></p>
 	</div>)
 
 export default Guide
