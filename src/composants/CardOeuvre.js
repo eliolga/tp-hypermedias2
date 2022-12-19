ed638f1c8ae889cff06377ed726cc00f5b2a5bbe
EliOlga <elisabeth.olga@gmail.com>
2022-12-18 23:34:38 -0500

    Refactoring

--- src/composants/CardOeuvre.js
+++ src/composants/CardOeuvre.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Card, Image} from 'semantic-ui-react'
 
 const CardOeuvre = (props) => (
-	<Card>
+	<Card >
 		<Image src={"https://macrepertoire.macm.org/media/oeuvres/collections/" + props.oeuvre.dateAcquisition + "/M/" + props.oeuvre.numero + "_IN001.jpg"} wrapped ui={false}/>
 		<Card.Content>
 			<Card.Header>{props.oeuvre.titre}</Card.Header>
